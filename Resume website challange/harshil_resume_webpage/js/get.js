b41a9d8ed847bf29f090b62b4fa48e707b884dab
harshil6009 <97859556+harshil6009@users.noreply.github.com>
2022-07-05 14:22:23 +1000

    removed the folder

--- Resume website challange/harshil_resume_webpage/js/get.js
+++
@@ -1,6 +0,0 @@
-const counter = document.getElementById("visits")
-let res = ""
-
-fetch('https://whz247oqhc.execute-api.us-east-1.amazonaws.com/first/')
-  .then(response => response.json())
-  .then(data => counter.textContent = data);
