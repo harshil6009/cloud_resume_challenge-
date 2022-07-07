2ff86263f3ed51bbeef390dd5ec44ba8c6e99764
harshil6009 <97859556+harshil6009@users.noreply.github.com>
2022-07-07 16:22:42 +1000

    terraform EC 2 code

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,27 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
+
     }
   }
+  backend "s3" {
+    bucket = "harshilvaghela.com"
+    key    = "terraform_record.tfstate"
+    region = "ap-southeast-2"
+  }
+  required_version = ">= 1.2.0"
+}
 
-  required_version = ">=0.14.9"
+provider "aws" {
+  region = "ap-southeast-2"
+}
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-07620139298af599e"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0882c00d94e1af5f7"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
